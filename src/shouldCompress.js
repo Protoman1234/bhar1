8b2234ba01ed001ec007caef7c8348109ee320fe
himshim26 <97538806+himshim@users.noreply.github.com>
2023-05-30 12:26:25 +0530

    Update shouldCompress.js

--- src/shouldCompress.js
+++ src/shouldCompress.js
@@ -1,6 +1,3 @@
-const MIN_COMPRESS_LENGTH = 1024;
-const MIN_TRANSPARENT_COMPRESS_LENGTH = MIN_COMPRESS_LENGTH * 100;
-
 function shouldCompress(req) {
   const { originType, originSize, webp } = req.params;
 
@@ -13,11 +10,27 @@ function shouldCompress(req) {
   if (webp && originSize < MIN_COMPRESS_LENGTH) {
     return false;
   }
-  if (!webp && (originType.endsWith('png') || originType.endsWith('gif')) && originSize < MIN_TRANSPARENT_COMPRESS_LENGTH) {
-    return false;
+  if (
+    originType.endsWith('png') ||
+    (originType.endsWith('gif') && originSize < MIN_TRANSPARENT_COMPRESS_LENGTH)
+  ) {
+    return shouldCompressSmallImage(req);
   }
 
   return true;
+}
+
+function shouldCompressSmallImage(req) {
+  const { originType, originSize } = req.params;
+
+  if (originType.endsWith('gif') && originSize < 10240) {
+    return true;
+  }
+  if (originType.endsWith('png') && originSize < 20480) {
+    return true;
+  }
+
+  return false;
 }
 
 module.exports = shouldCompress;
