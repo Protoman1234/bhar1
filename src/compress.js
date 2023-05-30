c39d46c50424ce6a142a51357cf2ab210c52b7f1
himshim26 <97538806+himshim@users.noreply.github.com>
2023-05-30 06:30:01 +0530

    Update compress.js

--- src/compress.js
+++ src/compress.js
@@ -1,8 +1,8 @@
-const sharp = require('sharp')
-const redirect = require('./redirect')
+const sharp = require('sharp');
+const redirect = require('./redirect');
 
 function compress(req, res, input) {
-  const format = req.params.webp ? 'webp' : 'jpeg'
+  const format = req.params.webp ? 'webp' : 'jpeg';
 
   sharp(input)
     .grayscale(req.params.grayscale)
@@ -12,16 +12,16 @@ function compress(req, res, input) {
       optimizeScans: true
     })
     .toBuffer((err, output, info) => {
-      if (err || !info || res.headersSent) return redirect(req, res)
+      if (err || !info || res.headersSent) {
+        return redirect(req, res);
+      }
 
-      res.setHeader('content-type', `image/${format}`)
-      res.setHeader('content-length', info.size)
-      res.setHeader('x-original-size', req.params.originSize)
-      res.setHeader('x-bytes-saved', req.params.originSize - info.size)
-      res.status(200)
-      res.write(output)
-      res.end()
-    })
+      res.setHeader('content-type', `image/${format}`);
+      res.setHeader('content-length', info.size);
+      res.setHeader('x-original-size', req.params.originSize);
+      res.setHeader('x-bytes-saved', req.params.originSize - info.size);
+      res.status(200).send(output);
+    });
 }
 
-module.exports = compress
+module.exports = compress;
