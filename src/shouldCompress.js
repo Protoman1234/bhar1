a7f376be3db2c14ac873c2975a369214941cc389
himshim26 <97538806+himshim@users.noreply.github.com>
2023-05-30 11:20:40 +0530

    Update shouldCompress.js

--- src/shouldCompress.js
+++ src/shouldCompress.js
@@ -1,6 +1,3 @@
-const MIN_COMPRESS_LENGTH = 1024;
-const MIN_TRANSPARENT_COMPRESS_LENGTH = MIN_COMPRESS_LENGTH * 100;
-
 function shouldCompress(req) {
   const { originType, originSize, webp } = req.params;
 
@@ -13,11 +10,31 @@ function shouldCompress(req) {
   if (webp && originSize < MIN_COMPRESS_LENGTH) {
     return false;
   }
-  if (!webp && (originType.endsWith('png') || originType.endsWith('gif')) && originSize < MIN_TRANSPARENT_COMPRESS_LENGTH) {
-    return false;
+  if (
+    originType.endsWith('png') ||
+    (originType.endsWith('gif') && originSize < MIN_TRANSPARENT_COMPRESS_LENGTH)
+  ) {
+    return shouldCompressSmallImage(req); // Call a separate function to handle small images
   }
 
-  return true;
+  return true; // Compress other image formats by default
+}
+
+function shouldCompressSmallImage(req) {
+  const { originType, originSize } = req.params;
+
+  // Determine your criteria for compressing small images
+  // Example: Compress GIFs with size less than 10KB
+  if (originType.endsWith('gif') && originSize < 10240) {
+    return true;
+  }
+
+  // Example: Compress transparent PNGs with size less than 20KB
+  if (originType.endsWith('png') && originSize < 20480) {
+    return true;
+  }
+
+  return false; // Do not compress if criteria are not met
 }
 
 module.exports = shouldCompress;
