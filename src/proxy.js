002ccf5c5afd74ba012752cc5f65e8162a01b982
himshim26 <97538806+himshim@users.noreply.github.com>
2023-05-30 06:30:55 +0530

    Update proxy.js

--- src/proxy.js
+++ src/proxy.js
@@ -1,10 +1,10 @@
-const request = require('request')
-const pick = require('lodash').pick
-const shouldCompress = require('./shouldCompress')
-const redirect = require('./redirect')
-const compress = require('./compress')
-const bypass = require('./bypass')
-const copyHeaders = require('./copyHeaders')
+const request = require('request');
+const pick = require('lodash').pick;
+const shouldCompress = require('./shouldCompress');
+const redirect = require('./redirect');
+const compress = require('./compress');
+const bypass = require('./bypass');
+const copyHeaders = require('./copyHeaders');
 
 function proxy(req, res) {
   request.get(
@@ -24,20 +24,22 @@ function proxy(req, res) {
       jar: true
     },
     (err, origin, buffer) => {
-      if (err || origin.statusCode >= 400) return redirect(req, res)
+      if (err || origin.statusCode >= 400) {
+        return redirect(req, res);
+      }
 
-      copyHeaders(origin, res)
-      res.setHeader('content-encoding', 'identity')
-      req.params.originType = origin.headers['content-type'] || ''
-      req.params.originSize = buffer.length
+      copyHeaders(origin, res);
+      res.setHeader('content-encoding', 'identity');
+      req.params.originType = origin.headers['content-type'] || '';
+      req.params.originSize = buffer.length;
 
       if (shouldCompress(req)) {
-        compress(req, res, buffer)
+        compress(req, res, buffer);
       } else {
-        bypass(req, res, buffer)
+        bypass(req, res, buffer);
       }
     }
-  )
+  );
 }
 
-module.exports = proxy
+module.exports = proxy;
