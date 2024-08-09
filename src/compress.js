6adcc9386641adaa7ab6bbc30ff415d643997f6d
Protoman1234 <rffrgtdgd@gmail.com>
2024-08-09 19:36:41 +0530

    Update compress.js

--- src/compress.js
+++ src/compress.js
@@ -9,7 +9,8 @@ function compress(req, res, input) {
     .toFormat(format, {
       quality: req.params.quality,
       progressive: true,
-      optimizeScans: true
+      optimizeScans: true,
+      concurrency: 1,
     })
     .toBuffer((err, output, info) => {
       if (err || !info || res.headersSent) {
