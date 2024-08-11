ebb11c34aa8b339555fc080019b5e0fa06ad1323
Protoman1234 <rffrgtdgd@gmail.com>
2024-08-11 15:18:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ const proxy = require('./src/proxy');
 const PORT = process.env.PORT || 8080;
 
 app.enable('trust proxy');
-app.get('/', params, proxy);
+app.get('/', proxy);
 app.get('/favicon.ico', (req, res) => res.status(204).end());
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
