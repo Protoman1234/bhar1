c98a6fd2eaab6e15fb0e3da0f2c1bffcdd94d80d
Protoman1234 <rffrgtdgd@gmail.com>
2024-08-11 19:54:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,4 +9,9 @@ const PORT = process.env.PORT || 8080;
 app.enable('trust proxy');
 app.get('/', params, proxy);
 app.get('/favicon.ico', (req, res) => res.status(204).end());
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).end('Internal Server Error');
+});
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
