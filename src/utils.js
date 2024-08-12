5a2ed9906594aa0e49a3874d8f87eb63ec6454c5
Protoman1234 <rffrgtdgd@gmail.com>
2024-08-12 11:39:24 +0530

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -3,4 +3,16 @@ function generateRandomIP() {
   return `${Math.floor(Math.random() * 256)}.${Math.floor(Math.random() * 256)}.${Math.floor(Math.random() * 256)}.${Math.floor(Math.random() * 256)}`;
 }
 
-module.exports = { generateRandomIP };
+function randomUserAgent() {
+  const userAgents = [
+    // Add a variety of common or randomized user agents
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Safari/605.1.15',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0',
+    // More user agents to increase randomness
+  ];
+
+  return userAgents[Math.floor(Math.random() * userAgents.length)];
+}
+
+module.exports = { generateRandomIP, randomUserAgent };
