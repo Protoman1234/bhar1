f4fa4f5ec6e689becc94c31a742a7c93b03772fc
Protoman1234 <rffrgtdgd@gmail.com>
2024-08-12 12:10:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const authenticate = require('./src/authenticate');
+//const authenticate = require('./src/authenticate');
 const proxy = require('./src/proxy');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.enable('trust proxy');
-app.get('/', authenticate, proxy);
+app.get('/', proxy);
 app.get('/favicon.ico', (req, res) => res.status(204).end());
 
 app.listen(PORT, () => {
