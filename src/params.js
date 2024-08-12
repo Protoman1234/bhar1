5540f44466d3329d1917c609d3b72bce0144bf0f
Protoman1234 <rffrgtdgd@gmail.com>
2024-08-12 11:47:41 +0530

    Update params.js

--- src/params.js
+++ src/params.js
@@ -1,15 +1,15 @@
-const { generateRandomIP } = require('./utils'); // Adjust the path as necessary
 const DEFAULT_QUALITY = 40;
+const { generateRandomIP, randomUserAgent } = require('./utils'); // Importing utility functions
 
 function params(req, res, next) {
   const { url, jpeg, bw, l } = req.query;
 
-  /*if (!url) {
-    // Randomize IP and send a generic message to avoid detection.
-   // req.params.randomIP = generateRandomIP();
-    return res.status(400).end('bandwidth-hero-proxy');
-  }*/
-  
+  if (!url) {
+    // Generate random IP and User-Agent in case of missing URL
+    req.headers['x-forwarded-for'] = generateRandomIP();
+    req.headers['user-agent'] = randomUserAgent();
+    return res.end('bandwidth-hero-proxy'); // This message can be customized or even omitted.
+  }
 
   const urls = Array.isArray(url) ? url.join('&url=') : url;
   const cleanedUrl = urls.replace(/http:\/\/1\.1\.\d\.\d\/bmi\/(https?:\/\/)?/i, 'http://');
@@ -18,6 +18,10 @@ function params(req, res, next) {
   req.params.webp = !jpeg;
   req.params.grayscale = bw !== '0';
   req.params.quality = parseInt(l, 10) || DEFAULT_QUALITY;
+
+  // Optional: Add additional anonymization or header randomization logic here
+  req.headers['x-forwarded-for'] = generateRandomIP();
+  req.headers['user-agent'] = randomUserAgent();
 
   next();
 }
