15d823e73ec017295318324b376879c51fb8d258
Protoman1234 <rffrgtdgd@gmail.com>
2024-08-14 09:37:06 +0530

    Create server.js

---
+++ functions/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+//const authenticate = require('./src/authenticate');
+const proxy = require('./src/proxy');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.enable('trust proxy');
+app.get('/', proxy);
+app.get('/favicon.ico', (req, res) => res.status(204).end());
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
